98e267b3bc40ff2c9aa1d1569795e1524cab5eb3
CandyPage <manuelagiraldotuberquia@gmail.com>
2023-03-02 03:47:51 -0500

    Urban Pro App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <header className="px-4 py-5 bg-white border-b">
         <div className="md:flex md:justify-between">
-            <h2 className="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
+            <h2 className="text-4xl text-pink-300 font-black text-center mb-5 md:mb-0">
                 Urban Pro App
             </h2>
 
