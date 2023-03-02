6de0f119ef946959220220308f44c0ca81fb3efc
CandyPage <manuelagiraldotuberquia@gmail.com>
2023-03-02 04:59:46 -0500

    Confirmar

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -38,8 +38,8 @@ const ConfirmarCuenta = () => {
 
   return (
     <>
-        <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma tu cuenta y Comienza a crear tus {''}
-            <span className="text-slate-700">proyectos</span>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma tu cuenta y Comienza en {''}
+            <span className="text-slate-700">Urban Pro App</span>
         </h1>
 
         <div className='mt-20 md:mt-10 shadow-lg px-5 py-10 rounded-xl bg-white'>
