8572415bc3517f4e74c87284051b0eb3f8bdb637
Urban-Pro <urbanproapp@outlook.com>
2023-03-09 02:34:38 -0500

    a

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import useAuth from '../hooks/useAuth'
 const Sidebar = () => {
 
   const { auth } = useAuth()
-  console.log(auth)
+  console.log(auth, "test")
 
   return (
     <aside className='md:w-1/3 lg:w-1/5 xl:w-1/6 px-5 py-10' >
