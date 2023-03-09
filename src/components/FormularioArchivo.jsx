929d7134181c15e1219648d5435a780f616d0c57
Urban-Pro <urbanproapp@outlook.com>
2023-03-09 07:34:15 -0500

    state

--- src/components/FormularioArchivo.jsx
+++ src/components/FormularioArchivo.jsx
@@ -1,37 +1,20 @@
 import React, { useState } from "react";
+import clienteAxios from '../config/clienteAxios'
 
 const FormularioArchivo = () => {
   const [archivo, setArchivo] = useState(null);
-  const [progreso, setProgreso] = useState(0);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  
     const formData = new FormData();
     formData.append("archivo", archivo);
-  
-    const xhr = new XMLHttpRequest();
-  
-    xhr.upload.addEventListener("progress", (event) => {
-      if (event.lengthComputable) {
-        const porcentaje = (event.loaded / event.total) * 100;
-        setProgreso(porcentaje.toFixed(0));
-      }
-    });
-  
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          alert("Archivo cargado correctamente");
-        } else {
-          alert("Error al cargar el archivo");
-        }
-      }
-    };
-  
-    xhr.open("POST", "/upload");
-    xhr.send(formData);
-  };  
+    try {
+      await clienteAxios.post("/upload", formData);
+      alert("Archivo cargado correctamente");
+    } catch (error) {
+      alert("Error al cargar el archivo");
+    }
+  };
 
   const handleFileChange = (e) => {
     setArchivo(e.target.files[0]);
@@ -50,20 +33,6 @@ const FormularioArchivo = () => {
           onChange={handleFileChange}
         />
       </div>
-      {progreso > 0 && (
-        <div className="progress mb-3">
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: `${progreso}%` }}
-            aria-valuenow={progreso}
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            {progreso}%
-          </div>
-        </div>
-      )}
       <button type="submit" className="btn btn-primary">
         Cargar archivo
       </button>
